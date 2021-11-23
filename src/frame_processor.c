6d6741b78db506444133803d803557b79cb1581c
Marcos Dominguez <mrds0690@gmail.com>
2021-11-23 06:52:58 -0300

    feat: evitar app resourses y checkear la no repeticion de instancias

--- src/frame_processor.c
+++ src/frame_processor.c
@@ -2,7 +2,7 @@
  * Authors: Marcos Raul Dominguez Shocron <mrds0690@gmail.com> - Pablo Javier Morzan
  * <pablomorzan@gmail.com> - Martin Julian Rios <jrios@fi.uba.ar>
  * Date: 19/11/2021
- * Version: 1.3
+ * Version: 1.4
  *===========================================================================*/
 
 /*=====[Inclusión de cabecera]=============================================*/
@@ -18,15 +18,8 @@
 #define POOL_PACKET_SIZE      MAX_BUFFER_SIZE
 #define POOL_PACKET_COUNT     (QUEUE_SIZE)
 #define POOL_SIZE_BYTES       (POOL_PACKET_SIZE * POOL_PACKET_COUNT * sizeof(char))
-
+#define INVALID_UART_ID       (-1)
 /*=====[Definición de tipos de datos privados]===================================*/
-/**
- * @brief Estructura con recursos para pasar a la tarea (puntero para pool y UART utilizada)
-*/
-typedef struct {
-    char *buffer;
-    uartMap_t uart;
-} app_resources_t;
 
 /*=====[Definición de variables globales publicas externas]=====================*/
 
@@ -45,44 +38,43 @@ static void C3_FRAME_PROCESSOR_Task(void *taskParmPtr);
 
 /*=====[Implementación de funciones públicas]=================================*/
 
-void C3_FRAME_PROCESSOR_Init(uartMap_t uart) {
-    app_resources_t *resources = pvPortMalloc(sizeof(app_resources_t));
-    configASSERT(resources != NULL);
-
-    resources->uart = uart;
-    //se reserva memoria dinámica para estructura de pool de cada instancia.
-    resources->buffer = (char *)pvPortMalloc(POOL_SIZE_BYTES);
-    configASSERT(resources->buffer != NULL);
-
-    BaseType_t xReturned = xTaskCreate(
-        C3_FRAME_PROCESSOR_Task,
-        (const char *)"Frame Processor",
-        configMINIMAL_STACK_SIZE * 3,
-        (void *) resources,
-        tskIDLE_PRIORITY + 1,
-        NULL
-        );
-    configASSERT(xReturned == pdPASS);
+bool_t C3_FRAME_PROCESSOR_Init(uartMap_t uart) {
+    bool_t ret;
+    static bool_t uart_used[UART_MAXNUM] = {FALSE};
+    if (uart_used[uart]) { 
+        ret = FALSE;
+    }
+    else {
+        ret = TRUE;
+        uart_used[uart] = TRUE;
+        BaseType_t xReturned = xTaskCreate(
+            C3_FRAME_PROCESSOR_Task,
+            (const char *)"Frame Processor",
+            configMINIMAL_STACK_SIZE * 3,
+            (void *) uart,
+            tskIDLE_PRIORITY + 1,
+            NULL
+            );
+        configASSERT(xReturned == pdPASS);
+    }
+    return ret;
 }
 
 /*=====[Implementación de funciones privadas]================================*/
 
 static void C3_FRAME_PROCESSOR_Task(void *taskParmPtr) {
-    app_resources_t *resources = (app_resources_t *) taskParmPtr;
-    uint8_t *memory_pool = resources->buffer;
-    uartMap_t uart = resources->uart;
-
-    vPortFree(resources);
+    uint8_t *memory_pool = (char *)pvPortMalloc(POOL_SIZE_BYTES);
+    configASSERT(memory_pool != NULL);
     QMPool pool;
     frame_class_t frame_obj;
     frame_obj.buffer_handler.pool = &pool;
     frame_obj.buffer_handler.queue = NULL,
     frame_obj.frame.data = NULL;
     frame_obj.frame.data_size = 0;
-    frame_obj.uart = uart;
+    frame_obj.uart = (uartMap_t) taskParmPtr;
     QMPool_init(&pool, (uint8_t *) memory_pool, POOL_SIZE_BYTES * sizeof(char), POOL_PACKET_SIZE);
 
-    C2_FRAME_PACKER_Init(&frame_obj.buffer_handler, uart); // Se inicializa el objeto de la instancia
+    C2_FRAME_PACKER_Init(&frame_obj.buffer_handler, frame_obj.uart); // Se inicializa el objeto de la instancia
 
     while (TRUE) {
         C2_FRAME_PACKER_Receive(&frame_obj.frame, &frame_obj.buffer_handler);
--- src/frame_transmit.c
+++ src/frame_transmit.c
@@ -2,7 +2,7 @@
  * Authors: Marcos Raul Dominguez Shocron <mrds0690@gmail.com> - Pablo Javier Morzan
  * <pablomorzan@gmail.com> - Martin Julian Rios <jrios@fi.uba.ar>
  * Date: 19/11/2021
- * Version: 1.3
+ * Version: 1.4
  *===========================================================================*/
 
 /*=====[Inclusión de cabecera]=============================================*/
